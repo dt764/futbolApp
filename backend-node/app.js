6fdf10c520a1855efb0acbc528c67918fd045b9d
dt764 <dt764@inlumine.ual.es>
2026-05-11 23:18:33 +0200

    Subida de programas de prueba, con CI

---
+++ backend-node/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'ok'
+  });
+});
+
+module.exports = app;
+
