3e2e2066654cb936a84c9c282ca39b4c577c38eb
dt764 <dt764@inlumine.ual.es>
2026-05-11 23:41:13 +0200

    Modificado configuración eslint

--- backend-node/eslint.config.js
+++ backend-node/eslint.config.js
@@ -1,10 +1,25 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
     rules: {
       // Add your custom rules here
+    },
+  },
+  {
+    files: ["**/*.test.js", "**/*.spec.js", "**/tests/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
     },
   },
 ];
