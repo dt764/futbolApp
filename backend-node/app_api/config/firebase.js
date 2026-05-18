79a4d472c640b805393d9b06ee931921c54ff1d7
dt764 <dt764@inlumine.ual.es>
2026-05-19 01:15:08 +0200

    Action updates

--- backend-node/app_api/config/firebase.js
+++ backend-node/app_api/config/firebase.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 let serviceAccount = null;
 
-if (process.env.FIREBASE_SERVICE_ACCOUNT_PATH) {
-  const filePath = path.resolve(process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
-  serviceAccount = require(filePath);
-} else if (process.env.FIREBASE_SERVICE_ACCOUNT_B64) {
+if (process.env.FIREBASE_SERVICE_ACCOUNT_B64) {
   const decoded = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_B64, 'base64').toString('utf-8');
   serviceAccount = JSON.parse(decoded);
+} else if (process.env.FIREBASE_SERVICE_ACCOUNT_PATH) {
+  const filePath = path.resolve(process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
+  serviceAccount = require(filePath);
 }
 
 if (!serviceAccount) {
