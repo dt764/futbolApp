4a712000e72fd445721a0a5f139d0d1a598d1b37
dt764 <dt764@inlumine.ual.es>
2026-05-29 04:31:46 +0200

    fix: set server URL via @Value from config server property

--- backend-java/player-service/src/main/java/com/futbolapp/playerservice/config/SwaggerConfig.java
+++ backend-java/player-service/src/main/java/com/futbolapp/playerservice/config/SwaggerConfig.java
@@ -2,15 +2,21 @@ package com.futbolapp.playerservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${springdoc.api-docs.server-url:https://tactixi.duckdns.org}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+            .addServersItem(new Server().url(serverUrl))
             .info(new Info()
                 .title("FutbolApp API - Player Service (DWSC)")
                 .version("1.0.0")
