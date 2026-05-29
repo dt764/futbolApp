0420607da42ee9f3ffb0a9dd9a58dec87ecb67bc
dt764 <dt764@inlumine.ual.es>
2026-05-29 04:13:53 +0200

    Modificacion config Swagger java

--- backend-java/comment-service/src/main/java/com/futbolapp/commentservice/config/SwaggerConfig.java
+++ backend-java/comment-service/src/main/java/com/futbolapp/commentservice/config/SwaggerConfig.java
@@ -2,16 +2,11 @@ package com.futbolapp.commentservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
-    @Value("${server.port:8082}")
-    private int port;
 
     @Bean
     public OpenAPI openAPI() {
@@ -19,9 +14,6 @@ public class SwaggerConfig {
             .info(new Info()
                 .title("FutbolApp API - Comment Service (DWSC)")
                 .version("1.0.0")
-                .description("API REST para gestión de comentarios - Backend Java"))
-            .addServersItem(new Server()
-                .url("http://localhost:" + port)
-                .description("Servidor local"));
+                .description("API REST para gestión de comentarios - Backend Java"));
     }
 }
